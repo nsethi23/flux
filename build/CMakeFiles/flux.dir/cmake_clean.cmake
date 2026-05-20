0d14d6ddcb28c9dcd0c3f6a2e9f79fa6bac1d303
nsethi23 <165092420+nsethi23@users.noreply.github.com>
2026-05-20 21:20:36 +0000

    feat: initial CMake project structure and hello world

---
+++ build/CMakeFiles/flux.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flux.dir/src/main.cpp.o"
+  "CMakeFiles/flux.dir/src/main.cpp.o.d"
+  "flux"
+  "flux.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/flux.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Flux order book \n";
+    return 0;
+}
