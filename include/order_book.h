4adc3e90da786c4e082c1770d1175890b581d1f7
nsethi23 <165092420+nsethi23@users.noreply.github.com>
2026-05-22 16:36:08 +0000

    cd .. git add . git commit -m "perf: pool allocator, p999 improved to 5850ns from 19156ns" git push

--- include/order_book.h
+++ include/order_book.h
@@ -2,9 +2,10 @@
 
 #include <map>
 #include <cstdint>
+#include <unordered_map>
 #include "order.h"
 #include "price_level.h"
-#include <unordered_map>
+#include "pool_allocator.h"
 
 struct OrderLocation {
     Side side;
@@ -25,4 +26,6 @@ private:
     std::map<int64_t, PriceLevel, std::greater<int64_t>> bids;
     std::map<int64_t, PriceLevel> asks;
     std::unordered_map<uint64_t, OrderLocation> order_map_;
+    static constexpr size_t MAX_ORDERS = 1'000'000;
+    PoolAllocator<Order, MAX_ORDERS> pool_;
 };
